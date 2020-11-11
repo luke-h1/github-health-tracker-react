5b52bbbbf72d9c773bb28a83290e816a635ac2f0
luke-h1 <luke.howsam@yahoo.com>
2020-11-11 13:03:03 +0000

    fix typo with folder struct

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
-import Status from '../../Status/Status';
+import Status from '../../components/Status/Status';
 const Home = () => {
   return (
     <>
